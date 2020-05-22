a7346ed237d0701c99dfda5fe5f0c7f9d6b415b6
SIO-UAS <mr19b061@technikum-wien.at>
2020-05-22 12:04:14 +0200

    Update struct.h

--- struct.h
+++ struct.h
@@ -1,10 +1,10 @@
 struct desti{
-  char *city_n;
-  char *country;
-  char *iso;
-  float lat;
-  flaot lng;
-  int id;
-  short int cap;
-  int pop;
+  char *city_n; // City name
+  char *country; // country name
+  char *iso; // 3 digit ISO code
+  float lat; // Latitude
+  flaot lng; // Longitude
+  int id; // ID
+  short int cap; // Is capital city
+  int pop; // Population
 }
