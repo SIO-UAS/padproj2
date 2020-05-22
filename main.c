9a2f09d8849653f910976cdb9c8c3fe2b9bd9357
SIO-UAS <mr19b061@technikum-wien.at>
2020-05-22 15:35:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -30,6 +30,112 @@ char* myStrDub(char* s){
     return ret;
 }
 
+char* myStrRev(char* s){
+    char* ret = NULL;
+    int til = 0;
+    while(s[til] != '\0'){
+        til++;
+    }
+    ret = calloc(til+1, sizeof(char));
+    if(ret == NULL){
+        printf("Something went horible wrong");
+        return NULL;
+    }
+    for(int i = 0; i< til; i++){
+        ret[i] = s[(til-1)-i];
+    }
+    return ret;
+}
+
+int myStrLen(char* s){
+    int til = 0;
+    while(s[til] != '\0'){
+        til++;
+    }
+    return til;
+}
+
+char* myStrCut(char* s, int a, int b){
+    char* ret = NULL;
+    int max = 0;
+    int revc = 0;
+
+     //
+    if (a>b){
+      revc = 1;
+    int temp =0;
+    temp =b;
+    b = a;
+    a = temp;
+
+    }
+    else{
+      revc = 0;
+    }
+    a--;
+    max = myStrLen(s);
+
+    if(s == NULL){
+      return NULL;
+    }
+
+
+    if((a < 0 && b <0) || (a > max && b > max)){
+        ret = calloc(1, sizeof(char));
+        return ret;
+    }
+    if((a < 0 && b > max) || (a > max && b < 0)){
+      if(revc > 0){
+        ret = myStrRev(s);
+      }
+      else{
+        ret = myStrDub(s);
+      }
+      return ret;
+    }
+    if(a > max || b > max){
+      if(revc > 0 ){
+        a = max;
+//printf("a = max");
+      }
+      else{
+//printf("B = max");
+        b = max;
+      }
+    }
+    if(a < 0 || b < 0){
+      if(revc){
+    b = 0;
+      }
+      else{
+        a=0;
+      }
+
+    }
+
+
+        ret = calloc((b-a)+1, sizeof(char));
+        if(ret == NULL){
+            printf("Something went horibly wrong");
+            return NULL;
+        }
+    //printf("Cutting %s von %d bis %d\n",s ,a, b);
+        for(int i = 0; i< (b-a); i++){
+            ret[i] = s[i+a];
+//printf("¬%d, b=%d", (i+a), b);
+
+        }
+//printf("%s", ret);
+        if(revc > 0){
+          return myStrRev(ret);
+        }
+        else{
+          return ret;
+      }
+
+    return ret;
+}
+
 int main()
 {
     FILE *csvSt, *csvRo;
@@ -69,7 +175,8 @@ int main()
 
         char *field = strtok(buf, "\"\",");
         while (field) {
-
+            field = myStrCut(field, 2, myStrLen(field)-1);
+            
             if (field_count == 1) {
                 Staedte[row_count -2].city_n = myStrDub(field);
                 //printf("%s",Staedte[row_count -2].city_n);
@@ -96,7 +203,7 @@ int main()
             if (field_count == 10) {
                 Staedte[row_count -2].id = atoi(field);
             }
-                field = strtok(NULL, "\",");
+                field = strtok(NULL, ",");
                 field_count++;
             }
 
