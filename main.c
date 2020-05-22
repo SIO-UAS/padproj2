492db826ee12a714b5f33803e2cc5429789ae94d
SIO-UAS <mr19b061@technikum-wien.at>
2020-05-22 14:52:34 +0200

    Update main.c

--- main.c
+++ main.c
@@ -13,6 +13,23 @@ typedef struct {
   int pop;
 }desti;
 
+char* myStrDub(char* s){
+    char* ret = NULL;
+    int til = 0;
+    while(s[til] != '\0'){ //Mit sizeof(*s)/sizeof(char) hat nicht funktioniert und nur 1 ausgegeben :( (&s, s) haben immer acht ausgegeben
+        til++;
+    }
+    ret = calloc(til+1, sizeof(char));
+    if(ret == NULL){
+        printf("Something went horible wrong");
+        return NULL;
+    }
+    for(int i = 0; i< til; i++){
+        ret[i] = s[i];
+    }
+    return ret;
+}
+
 int main()
 {
     FILE *csvSt, *csvRo;
@@ -23,12 +40,14 @@ int main()
         printf("Fehler beim �ffnen der Datei");
     }
     desti* Staedte = NULL;
+    desti* test = NULL;
 
     Staedte = (desti*) realloc(Staedte, sizeof(desti));
 
     char buf[1024];
-    int row_count = 0;
+    long int row_count = 0;
     int field_count = 0;
+    int z = 0;
     while (fgets(buf, 1024, csvSt)) {
 
         field_count = 0;
@@ -37,29 +56,40 @@ int main()
         if (row_count == 1) {
             continue;
         }
-        Staedte = (desti*) realloc(Staedte,sizeof(desti)*(row_count-1));
+        test = (desti*) realloc(Staedte,sizeof(desti)*(row_count-1));
+        if(test == NULL){
+            break;
+        }
+        else{
+            Staedte = test;
+        }
+        if((row_count % 100)==1){
+            z++;
+        }
 
-        char *field = strtok(buf, ",");
+        char *field = strtok(buf, "\"\",");
         while (field) {
 
             if (field_count == 1) {
-                Staedte[row_count -2].city_n = field;
+                Staedte[row_count -2].city_n = myStrDub(field);
+                //printf("%s",Staedte[row_count -2].city_n);
             }
             if (field_count == 2) {
                 Staedte[row_count -2].lat = atof(field);
+                //printf("%f",Staedte[row_count -2].lat);
             }
             if (field_count == 3) {
                 Staedte[row_count -2].lng = atof(field);
             }
             if (field_count == 4) {
-                Staedte[row_count -2].country = field;
+                Staedte[row_count -2].country = myStrDub(field);
             }
             if (field_count == 6) {
-                Staedte[row_count -2].iso = field;
+                Staedte[row_count -2].iso = myStrDub(field);
             }
-            if (field_count == 8 && field == 'primary') {
+           /* if (field_count == 8 && field == 'primary') {
                 Staedte[row_count -2].cap = 1;
-            }
+            }*/
             if (field_count == 9) {
                 Staedte[row_count -2].pop = atoi(field);
             }
@@ -75,7 +105,7 @@ int main()
     fclose(csvSt);
     int x = 0;
     while(x <= row_count){
-        printf("%s %s %s %f %f %d %d/n",Staedte[x].city_n,Staedte[x].country,Staedte[x].iso,Staedte[x].lat,Staedte[x].lng,Staedte[x].pop,Staedte[x].id );
+        printf("%s %s %s %f %f %d %d\n",Staedte[x].city_n,Staedte[x].country,Staedte[x].iso,Staedte[x].lat,Staedte[x].lng,Staedte[x].pop,Staedte[x].id );
         x++;
     }
 
