10034ec8fdba4f3e6fcc15f84248f06114434521
SIO-UAS <mr19b061@technikum-wien.at>
2020-05-29 19:07:19 +0200

    Update mainlib.c

--- mainlib.c
+++ mainlib.c
@@ -1,9 +1,9 @@
 #include "depen.h"
 #include "csv.h"
 
-int searchid(desti *Staedte, int id){ // Wurst Käs Zenario jet
+int searchid(desti *Staedte, int id, int max){ // Wurst Käs Zenario jet
   int x = 0;
-  while(Staedte[x].id != 0 && Staedte[x].id != id){
+  while(x < max && Staedte[x].id != id){
     x++;
   }
   return x;
@@ -11,25 +11,31 @@ int searchid(desti *Staedte, int id){ // Wurst Käs Zenario jet
 
 
 int main(){
-  FILE *csvSt, *csvRo;
+  FILE *csvSt, *csvRo, *csvback;
 
   csvSt = fopen("worldcities.csv","r");
 
   if( csvSt == NULL){
       printf("Fehler beim �ffnen der Datei");
   }
+
+  printf("Geoefnet\n");
   desti* Staedte = NULL;
   int data_c = 0;
-  data_c = rcs(csvSt, Staedte);
-
+  printf("Daten Einlesen Starten\n");
+  printf("Daten Eingelesen %d Datensaetze gefunden Speicheradr: %p\n", data_c, Staedte);
+  Staedte = rcs(csvSt, Staedte, &data_c);
   fclose(csvSt);
+  printf("Daten Eingelesen %d Datensaetze gefunden Speicheradr: %p\n", data_c, Staedte);
+
+
   int x = 0;
   while(x <= data_c){
       printf("%s %s %s %f %f %d %d\n",Staedte[x].city_n,Staedte[x].country,Staedte[x].iso,Staedte[x].lat,Staedte[x].lng,Staedte[x].pop,Staedte[x].id );
       x++;
   }
   printf("\nSearching ID ...");
-  x = searchid(Staedte, 1124019423);
+  x = searchid(Staedte, 1124019423, data_c);
   printf("%s %s %s %f %f %d %d\n",Staedte[x].city_n,Staedte[x].country,Staedte[x].iso,Staedte[x].lat,Staedte[x].lng,Staedte[x].pop,Staedte[x].id );
 
 
