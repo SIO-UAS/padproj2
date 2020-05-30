e416cf0daec2d3278ed539bce1774629b881e430
SIO-UAS <mr19b061@technikum-wien.at>
2020-05-30 13:16:58 +0200

    Update search.c
    Added a searchfunction for the 3 digit ISO

--- search.c
+++ search.c
@@ -7,3 +7,25 @@ int searchid(desti *Staedte, int id, int max){ // Wurst Käs Zenario jet
   }
   return x;
 }
+
+
+desti* searchISO(desti *Staedte, char* iso, int max, int *rc){
+    desti* result = NULL;
+    rc = 0;
+    int x = 0;
+    while(x < max){
+      if(Staedte->iso[0] == iso[0]){
+        if(Staedte->iso[1] == iso[1]){
+          if(Staedte->iso[2] == iso[2]){
+            rc++;
+            result = (desti*) realloc(result, (*rc)*(sizeof(desti)));
+          }
+          else{}
+        }
+        else{}
+      }
+      else{}
+
+    }
+    return result;
+}
