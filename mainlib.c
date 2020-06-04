af2e51cf5979b0fcca56118d6930483d6af92d39
SIO-UAS <mr19b061@technikum-wien.at>
2020-06-04 11:09:10 +0200

    Update mainlib.c

--- mainlib.c
+++ mainlib.c
@@ -1,6 +1,7 @@
 #include "depen.h"
 #include "csv.h"
 #include "search.h"
+#include "sort.h"
 
 
 
@@ -25,55 +26,74 @@ int main(){
 
 
   int x = 0;
-  while(x <= data_c){
+/*  while(x <= data_c){
       printf("%s %s %s %f %f %d %d\n",Staedte[x].city_n,Staedte[x].country,Staedte[x].iso,Staedte[x].lat,Staedte[x].lng,Staedte[x].pop,Staedte[x].id );
       x++;
   }
+*/
   printf("\nSearching ID ...");
   x = searchid(Staedte, 1124019423, data_c);
   printf("%s %s %s %f %f %d %d\n",Staedte[x].city_n,Staedte[x].country,Staedte[x].iso,Staedte[x].lat,Staedte[x].lng,Staedte[x].pop,Staedte[x].id );
 
   int res = 0;
 
-  printf("Searching for ISO AUT... \n");
-  desti* sres = searchISO(Staedte, "AUT", data_c, &res);
+  printf("Searching for ISO USA... \n");
+  desti* sres = searchISO(Staedte, "USA", data_c, &res);
   if(sres == NULL){
     printf("Fehler bei der Suche!\n");
     return 0;
   }
   printf("Gefunden : %d Ergebnisse\n", res);
   x=0;
+/*
   while(x < res){
     printf("%s %s %s %f %f %d %d\n",sres[x].city_n,sres[x].country,sres[x].iso,sres[x].lat,sres[x].lng,sres[x].pop,sres[x].id );
     x++;
   }
-
+*/
   res = 0;
   printf("Searching for City(s) named Vienna... \n");
-  sres = searchName(Staedte, "Vienna", data_c, &res);
+  //sres = searchName(Staedte, "Vienna", data_c, &res);
   if(sres == NULL){
     printf("Fehler bei der Suche!\n");
     return 0;
   }
   printf("Gefunden : %d Ergebnisse\n", res);
   x=0;
+/*
   while(x < res){
     printf("%s %s %s %f %f %d %d\n",sres[x].city_n,sres[x].country,sres[x].iso,sres[x].lat,sres[x].lng,sres[x].pop,sres[x].id );
     x++;
   }
-
+*/
   res = 0;
   printf("Searching for City(s) named Paris... \n");
-  sres = searchName(Staedte, "Paris", data_c, &res);
+  sres = searchName(Staedte, "Medellin", data_c, &res);
   if(sres == NULL){
     printf("Fehler bei der Suche!\n");
     return 0;
   }
   printf("Gefunden : %d Ergebnisse\n", res);
   x=0;
+/*
   while(x < res){
     printf("%s %s %s %f %f %d %d\n",sres[x].city_n,sres[x].country,sres[x].iso,sres[x].lat,sres[x].lng,sres[x].pop,sres[x].id );
     x++;
   }
+*/
+  printf("Sortieren ....\n");
+/*  desti* sorted = NULL;
+  sorted = inSort_id(Staedte, (data_c-2));
+  while(x <= (data_c-2)){
+      printf("%s %s %s %f %f %d %d\n",sorted[x].city_n,sorted[x].country,sorted[x].iso,sorted[x].lat,sorted[x].lng,sorted[x].pop,sorted[x].id );
+      x++;
+  }
+  */
+  desti* sorted = NULL;
+    sorted = inSort_pop(Staedte, (data_c-2), 1);
+    while(x <= (data_c-2)){
+        printf("%-30s %-25s %-9d\n",sorted[x].city_n,sorted[x].country,sorted[x].pop);
+        x++;
+    }
 
 }
