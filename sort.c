272acf18c2335e5f62fa8911224058b254cc46cf
SIO-UAS <mr19b061@technikum-wien.at>
2020-06-04 17:05:56 +0200

    Update sort.c
    BucketSort für Staedte Namen noch instabil :-1:  Nicht Verwenden :heavy_exclamation_mark: :warning:

--- sort.c
+++ sort.c
@@ -44,3 +44,49 @@ desti* inSort_pop(desti *Staedte, int max, int dir){ //insertion sort
   }
   return Staedte;
 }
+
+desti* aBucketSort(desti *Steadte, int max){
+  desti* result = NULL;
+  desti* test = NULL;
+  desti** buckets = (desti**)malloc(26*sizeof(desti*));
+  int into;
+  int n[26];
+  for(int i = 0; i < 26; i++){
+    n[i] = 0;
+  }
+
+    for(int i = 0; i<max; i++){
+
+      if(Steadte[i].city_n != NULL){
+        if(Steadte[i].city_n[0] > 96){
+          into = (int)(Steadte[i].city_n[0] - 'a');
+        }
+        else{
+          into = (int)(Steadte[i].city_n[0] - 'A');
+        }
+        n[into]++;
+        test = realloc(buckets[into], sizeof(desti)*n[into]);
+        if(test != NULL){
+          buckets[into] = test;
+          buckets[into][n[into]] = Steadte[i];
+        }
+
+      }
+
+    }
+
+    result = (desti*)malloc(sizeof(desti*) * max);
+    int z = 0;
+    for(int i=0; i < 26; i++){
+      for(int b=0; b< n[i]; b++){
+        if(z < max){
+          result[z]=buckets[i][b];
+          z++;
+        }
+        else{
+          printf("I have a Question: HOW?");
+        }
+      }
+    }
+    return result;
+}
