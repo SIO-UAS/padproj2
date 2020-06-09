01ba850ddf58a7bdd33aed1c8e6389831da00d91
SIO-UAS <mr19b061@technikum-wien.at>
2020-06-09 11:01:33 +0200

    Update mainlib.c

--- mainlib.c
+++ mainlib.c
@@ -2,6 +2,8 @@
 #include "csv.h"
 #include "search.h"
 #include "sort.h"
+#include "del.h"
+#include "disp.h"
 
 
 
@@ -26,18 +28,19 @@ int main(){
 
 
   int x = 0;
-/*  while(x <= data_c){
+  /*while(x <= data_c){
       printf("%s %s %s %f %f %d %d\n",Staedte[x].city_n,Staedte[x].country,Staedte[x].iso,Staedte[x].lat,Staedte[x].lng,Staedte[x].pop,Staedte[x].id );
       x++;
-  }
-*/
+  }*/
+  disp_dlistad(Staedte, data_c);
+
   printf("\nSearching ID ...");
   x = searchid(Staedte, 1124019423, data_c);
   printf("%s %s %s %f %f %d %d\n",Staedte[x].city_n,Staedte[x].country,Staedte[x].iso,Staedte[x].lat,Staedte[x].lng,Staedte[x].pop,Staedte[x].id );
 
   int res = 0;
 
-  printf("Searching for ISO USA... \n");
+  /*printf("Searching for ISO USA... \n");
   desti* sres = searchISO(Staedte, "USA", data_c, &res);
   if(sres == NULL){
     printf("Fehler bei der Suche!\n");
@@ -45,12 +48,16 @@ int main(){
   }
   printf("Gefunden : %d Ergebnisse\n", res);
   x=0;
+  //delete_all(sres, res);
+  sres =NULL;
+  */
 /*
   while(x < res){
     printf("%s %s %s %f %f %d %d\n",sres[x].city_n,sres[x].country,sres[x].iso,sres[x].lat,sres[x].lng,sres[x].pop,sres[x].id );
     x++;
   }
 */
+  /*
   res = 0;
   printf("Searching for City(s) named Vienna... \n");
   //sres = searchName(Staedte, "Vienna", data_c, &res);
@@ -60,6 +67,7 @@ int main(){
   }
   printf("Gefunden : %d Ergebnisse\n", res);
   x=0;
+  */
 /*
   while(x < res){
     printf("%s %s %s %f %f %d %d\n",sres[x].city_n,sres[x].country,sres[x].iso,sres[x].lat,sres[x].lng,sres[x].pop,sres[x].id );
@@ -68,13 +76,15 @@ int main(){
 */
   res = 0;
   printf("Searching for City(s) named Paris... \n");
-  sres = searchName(Staedte, "Medellin", data_c, &res);
+  /*sres = searchName(Staedte, "Medellin", data_c, &res);
   if(sres == NULL){
     printf("Fehler bei der Suche!\n");
     return 0;
   }
   printf("Gefunden : %d Ergebnisse\n", res);
   x=0;
+  //delete_all(sres, res);
+  sres = NULL;*/
 /*
   while(x < res){
     printf("%s %s %s %f %f %d %d\n",sres[x].city_n,sres[x].country,sres[x].iso,sres[x].lat,sres[x].lng,sres[x].pop,sres[x].id );
@@ -90,10 +100,25 @@ int main(){
   }
   */
   desti* sorted = NULL;
-    sorted = inSort_pop(Staedte, (data_c-2), 1);
-    while(x <= (data_c-2)){
+    //sorted = inSort_pop(Staedte, (data_c-2), 1);
+    /*while(x <= (data_c-2)){
         printf("%-30s %-25s %-9d\n",sorted[x].city_n,sorted[x].country,sorted[x].pop);
         x++;
-    }
+    }*/
+    x=0;
+    //disp_dlistad(Staedte, data_c);
+    sorted = bucket_sb(Staedte, (data_c-2), 0);
+    disp_dlistad(sorted, (data_c-2));
+  /*  while(sorted[x].city_n != NULL){
 
+        printf("%-30s %-25s %-9d\n",sorted[x].city_n,sorted[x].country,sorted[x].id);
+        x++;
+
+
+    }*/
+
+    desti* q = calloc(15, sizeof(desti));
+    //delete_all(sorted, (data_c-2));
+    printf("gebe Speicher wieder frei:");
+    delete_all(Staedte, (data_c-2));
 }
