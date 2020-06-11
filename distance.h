9935d618512f5f7337b7728a23cd8bc3e9f0f5b0
ArtnerA <65605967+ArtnerA@users.noreply.github.com>
2020-06-11 14:35:46 +0200

    Create distance.h

---
+++ distance.h
@@ -0,0 +1,6 @@
+#ifndef DISTANCE_H_INCLUDED
+#define DISTANCE_H_INCLUDED
+double DistanceAB(double latitudeA, double latitudeB, double longitudeA, double longitudeB);
+
+
+#endif // DISTANCE_H_INCLUDED
