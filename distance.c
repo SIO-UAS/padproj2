c069e3c8394642ffb0c769db2d76778ef7b0fa78
SIO-UAS <mr19b061@technikum-wien.at>
2020-06-12 19:56:10 +0200

    Update distance.c

--- distance.c
+++ distance.c
@@ -4,7 +4,7 @@
 
 double DistanceAB (double latitudeA, double latitudeB, double longitudeA, double longitudeB){ //calculates the distance between two points on the globe
 
-    const double M_PI = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
+    //const double M_PI = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
     double latA = (latitudeA * (M_PI/180)), latB = (latitudeB * (M_PI/180)), lonA = (longitudeA * (M_PI/180)), lonB = (longitudeB * (M_PI/180));
     double distance = 0;
     const double R = 6372.795477598;
