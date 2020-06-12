5843c1706bb7037d8d4004d97f3423a3bcff833b
SIO-UAS <mr19b061@technikum-wien.at>
2020-06-12 15:33:16 +0200

    Create csv.h

---
+++ csv.h
@@ -0,0 +1,7 @@
+desti* rcs(FILE*, desti*, int*);
+char* myStrDub(char*);
+char* myStrRev(char*);
+int myStrLen(char*);
+char* myStrCut(char*, int, int);
+char* defaultsFile(char* );
+void append_csv(char*, desti*, int);
