f142ab7fe27920ca5a431d3971e266211f372633
SIO-UAS <mr19b061@technikum-wien.at>
2020-06-12 14:56:37 +0200

    Update disp.c

--- disp.c
+++ disp.c
@@ -1,9 +1,9 @@
 #include "depen.h"
 void disp_dlistad(desti* to, int count){ //display desti list all data
   int x = 0;
-  while(x <= count){
+  while(x < count){
       if(to[x].city_n!= NULL){
-        printf("%s %s %s %f %f %d %d\n",to[x].city_n,to[x].country,to[x].iso,to[x].lat,to[x].lng,to[x].pop,to[x].id );
+        printf("%-25s %-15s %-5s %-12f %-12f %-8d %d\n",to[x].city_n,to[x].country,to[x].iso,to[x].lat,to[x].lng,to[x].pop,to[x].id );
         x++;
       }
       else{
@@ -15,7 +15,7 @@ void disp_dlistad(desti* to, int count){ //display desti list all data
 
 void printMap_Link(float lat, float log){ //Print Openstreetmap Link to Cordinates
 
-  printf("https://www.openstreetmap.org/search?query=%f%%2C%f", lat, log);
+  printf("https://www.openstreetmap.org/search?query=%f%%2C%f\n", lat, log);
 
   return;
 }
